657503579c0dcf801fbeded2ce003b8b31201244
stepanovep <stepanovep@gmail.com>
2021-12-27 00:25:11 +0300

    connection to Astra DB Cassandra server

--- src/main/java/ru/stepanovep/Application.java
+++ src/main/java/ru/stepanovep/Application.java
@@ -3,8 +3,15 @@ package ru.stepanovep;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import ru.stepanovep.connection.DataStaxAstraProperties;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -13,5 +20,12 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+    }
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
 }

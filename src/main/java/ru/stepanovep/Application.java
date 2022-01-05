5489c4808141db28dcaf08772404b648d55c0188
stepanovep <stepanovep@gmail.com>
2022-01-05 23:42:44 +0300

    connect to local cassandra

--- src/main/java/ru/stepanovep/Application.java
+++ src/main/java/ru/stepanovep/Application.java
@@ -3,15 +3,12 @@ package ru.stepanovep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import ru.stepanovep.connection.DataStaxAstraProperties;
 import ru.stepanovep.loader.author.Author;
 import ru.stepanovep.loader.author.AuthorRepository;
 
 import javax.annotation.PostConstruct;
-import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
@@ -33,11 +30,5 @@ public class Application {
         author.setPersonalName("personalName");
 
         authorRepository.save(author);
-    }
-
-    @Bean
-    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-        Path bundle = astraProperties.getSecureConnectBundle().toPath();
-        return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
 }
